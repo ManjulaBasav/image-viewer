2a4d954bc1b2a60333daa687213260d2c8ac1a03
ManjulaHH <manjula.oct7@gmail.com>
2018-11-11 17:03:42 +0530

    Added Header

---
+++ src/common/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+ class Header extends Component {
+    render() {
+        return (
+            <div>
+                <header className="app-header">
+                    <div className="header-name">
+                        Image Viewer                                                    
+                    </div>
+                </header>
+            </div>
+        )
+    }
+}
+ export default Header; 
